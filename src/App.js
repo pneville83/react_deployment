9ef686a8a033182cd4af380f976efbe35e5c7508
Peter Neville <neville.peter.2000@gmail.com>
2022-09-18 12:51:17 -0500

    formulario con formik y testing react native

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Formulario from './Form/Form'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
